0f798fb3c05301dc0f18c00c327bb4dab129be86
NightHegg_Secondary <61695767+NightHegg@users.noreply.github.com>
2021-04-02 14:43:30 +0300

    cur_update

--- nsm_ode_2021/homework/main.py
+++ nsm_ode_2021/homework/main.py
@@ -25,19 +25,52 @@ def runge_kutta(task, method, tau):
     print(y[-1], exp(5))
     print(np.isclose(exp(5), y[-1]))
 
+def runge_kutta_template(function, tau, x_0, y_0, a, c):
+        k = []
+        for i in range(c.size):
+            k.append(
+                        function(
+                            x_0 + c[i] * tau, 
+                            y_0 + tau * np.dot(np.array(k[:i]), a[i, :i])
+                        )
+                    )
+        return np.array(k)
+
+def runge_kutta(f, tau, x_0, y_0, a, c):
+    x = t[0]
+    y = np.array([task['init_cond']])
+    tau = 0.05
+    x, y = [x_0], [y_0]
+    amnt_small_steps = 2
+    amnt_iters = 0
+    while True:
+        y_small_steps = [y_0]
+        x = x_0
+        for i in range(amnt_small_steps):
+            k = runge_kutta_template(f, tau, x, y_small_steps[-1], a, c)
+            y = np.append(y_small_steps, y_small_steps[-1] + tau * k @ b)
+            x += tau
+        
+        k = runge_kutta_template(f, amnt_small_steps * tau, x, y_0, a, c)
+        y_big_step = y_0 + amnt_small_steps * tau * k @ b
+
+        
+
+
+
 
 class task_01():
     def __init__(self, t):
         self.t = t
         self.y_current = np.array([exp(sin(self.t[0] ** 2)), exp(cos(self.t[0] ** 2))])
         func(self.t[0], self.y_current)
-
+ 
 if __name__ == "__main__":
     N_var = 2
     t_1 = N_var * 0.1
 
     task = {
-        'main': {
+        'primary': {
             'func':      lambda x, y: np.array([2*x, -2*x]) * y * np.log(np.amax(np.vstack((np.flip(y, 0), np.array([1e-3, 1e-3]))), 0)),
             'init_cond': np.array([exp(sin(t_1 ** 2)), exp(cos(t_1** 2))]),
             'interval':  np.array([t_1, t_1 + 4])
